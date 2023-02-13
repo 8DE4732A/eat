7da3976b18ca3b633d43c4d75e9c99701754c702
liuping <liuping@liuping.win>
2023-02-13 08:43:09 +0800

    1

---
+++ req.js
@@ -0,0 +1,13 @@
+let headers = $request.headers;
+if(headers['ifuli-longitude'] != undefined && headers['ifuli-longitude'] != null) {
+    headers['ifuli-longitude'] = '121.488100';
+}
+if(headers['ifuli-latitude'] != undefined && headers['ifuli-latitude'] != null) {
+    headers['ifuli-latitude'] = '31.243084';
+}
+if(headers['ifuli-location'] != undefined && headers['ifuli-location'] != null) {
+    headers['ifuli-latitude'] = '31.243084,121.488100';
+}
+if($request.url.indexOf('eat.zuifuli.com/ctwrapper') != -1) {
+    headers['If-None-Match'] = null
+}
