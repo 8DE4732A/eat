5b8e75a5e244b396b7cf266e8c1a5ed1607be60f
liuping <liuping@liuping.win>
2023-02-15 09:15:01 +0800

    1

--- rsp.js
+++ rsp.js
@@ -2,7 +2,7 @@ if ($request.url.indexOf('eat.zuifuli.com/ctwrapper') != -1) {
     if($response.status == 200){
         $notification.post("最福利已fake", "", "");
         // var index = $response.body.indexOf('<script>');
-        var body = "<!DOCTYPE html><html><head><title>test</title></head><body><p></p><script>document.addEventListener('DOMContentLoaded',(event)=>{iHealthBridge.doAction('locate',{},function(ret,suc){var p=document.querySelector('p');p.value=JSON.stringify(ret)})});</script></body></html>";
+        var body = "<!DOCTYPE html><html><head><title>test</title></head><body><p></p><script>document.addEventListener('DOMContentLoaded',(event)=>{iHealthBridge.doAction('locate',{},function(ret,suc){var p=document.querySelector('p');p.text=JSON.stringify(ret)})});</script></body></html>";
         console.log('body', body);
         $done({
             status: 200, 
