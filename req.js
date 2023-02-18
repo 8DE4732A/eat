921905b5f3c0e34c62db8826e04534a1898730c9
liuping <liuping@liuping.win>
2023-02-18 13:17:07 +0800

    他们偷懒了

--- req.js
+++ req.js
@@ -1,12 +1,12 @@
 let headers = $request.headers;
 if(headers['ifuli-longitude'] != undefined && headers['ifuli-longitude'] != null) {
-    headers['ifuli-longitude'] = '121.488100';
+    headers['ifuli-longitude'] = '121.48789';
 }
 if(headers['ifuli-latitude'] != undefined && headers['ifuli-latitude'] != null) {
-    headers['ifuli-latitude'] = '31.243084';
+    headers['ifuli-latitude'] = '31.24309';
 }
 if(headers['ifuli-location'] != undefined && headers['ifuli-location'] != null) {
-    headers['ifuli-location'] = '31.243084,121.488100';
+    headers['ifuli-location'] = '31.24309,121.48789';
 }
 if($request.url.indexOf('eat.zuifuli.com/ctwrapper') != -1) {
     headers['If-None-Match'] = null
--- rsp.js
+++ rsp.js
@@ -1,9 +1,8 @@
 if ($request.url.indexOf('eat.zuifuli.com/ctwrapper') != -1) {
     if($response.status == 200){
         $notification.post("最福利已fake", "", "");
-        // var index = $response.body.indexOf('<script>');
-        var body = "<!DOCTYPE html><html><head><title>test</title><script src='//cdn.jsdelivr.net/npm/eruda'></script><script>eruda.init();</script></head><body><p></p><script>document.addEventListener('DOMContentLoaded',(event)=>{iHealthBridge.doAction('locate',{},function(ret,suc){var p=document.querySelector('p');if(suc){p.value=JSON.stringify(ret)}else{p.value='failed'}})});</script></body></html>";
-        console.log('body', body);
+        var h = "<script>window.iHealthBridge._doAction=window.iHealthBridge.doAction;window.iHealthBridge.doAction=function(f,p,c){if(f==='locate'){c({'adcode':'310100','city':'上海市','address':'外白渡桥','date':1676696166.1296182,'lat':31.24309,'lng':121.48789,'poi':'外白渡桥'},true)}}navigator.geolocation._getCurrentPosition=navigator.geolocation.getCurrentPosition;navigator.geolocation.getCurrentPosition=function(f){navigator.geolocation._getCurrentPosition(p=>{Object.defineProperty(p.coords,'longitude',{get:function(){return 121.48789}});Object.defineProperty(p.coords,'latitude',{get:function(){return 31.24309}});console.info('fake',p);f(p)})}</script>";
+        var body = $response.body.replace('<script></script>', h)
         $done({
             status: 200, 
             body: body
